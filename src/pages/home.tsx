a9e5ad47a4eea2b84956253e071cc9cb909e436e
Chris Rubio <crubio@twilio.com>
2025-08-02 12:52:14 -0700

    finish touches - dashboard is now home page prime

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,7 @@
-import { getHomePageVariation } from "utils/ab-testing";
-import CurrentHome from "./current-home";
 import DashboardHome from "./dashboard-home";
-import DiscoveryHome from "./discovery-home";
-import MinimalistHome from "./minimalist-home";
-import DataRichHome from "./data-rich-home";
 
 const Home = () => {
-  const variation = getHomePageVariation();
-  
-  // Render the appropriate home page variation
-  switch (variation) {
-    case 'dashboard':
-      return <DashboardHome />;
-    case 'discovery':
-      return <DiscoveryHome />;
-    case 'minimalist':
-      return <MinimalistHome />;
-    case 'data-rich':
-      return <DataRichHome />;
-    case 'current':
-    default:
-      return <CurrentHome />;
-  }
+  return <DashboardHome />;
 };
 
 export default Home;
