2ee4940ba6cfce87acf6176e1375e7aa9d7530ae
Chris Rubio <crubio@twilio.com>
2025-08-05 22:48:11 -0700

    test after removing suspect imports

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,4 @@
-import { getHomePageVariation } from 'utils/ab-testing';
 import DashboardHome from "./dashboard-home";
-import DiscoveryHome from "./discovery-home";
-import DataRichHome from "./data-rich-home";
 
 const Home = () => {
   console.log('Home component starting...');
