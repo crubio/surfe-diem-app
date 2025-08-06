af27e7ec941226a70d71e4de740130aab9532d75
Chris Rubio <crubio@twilio.com>
2025-08-05 21:56:44 -0700

    new branch for main w/hotfix"

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,4 @@
-import { getHomePageVariation } from 'utils/ab-testing';
 import DashboardHome from "./dashboard-home";
-import DiscoveryHome from "./discovery-home";
-import DataRichHome from "./data-rich-home";
 
 const Home = () => {
   return <DashboardHome />;
