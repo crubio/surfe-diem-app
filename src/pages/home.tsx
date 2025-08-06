ed4b5567ca3f4c9f66cca95d531fc06765ce627e
Chris Rubio <crubio@twilio.com>
2025-08-05 22:34:48 -0700

    systematically testing adding back AB comps after last working deploy

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,17 @@ import DiscoveryHome from "./discovery-home";
 import DataRichHome from "./data-rich-home";
 
 const Home = () => {
-  return <DashboardHome />;
+  try {
+    const variation = getHomePageVariation();
+    console.log('A/B test variation:', variation);
+    
+    // For now, just return DashboardHome regardless of variation
+    // This tests if the A/B testing logic itself works
+    return <DashboardHome />;
+  } catch (error) {
+    console.warn('Error in Home component, falling back to dashboard:', error);
+    return <DashboardHome />;
+  }
 };
 
 export default Home;
